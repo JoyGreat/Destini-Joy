c540540b95f119c06a8cfa54dada9acb1d0d5787
Joy <joy@joy.co>
2023-02-04 00:27:37 +0100

    Complete

---
+++ Destini Joy/Model/Story.swift
@@ -0,0 +1,20 @@
+//
+//  Story.swift
+//  Destini Joy
+//
+//  Created by Anton on 03.02.2023.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
+
+
+
+
